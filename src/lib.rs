9dbaf34cd01dfe1fc8c84e7d56c769974bd252a0
Lozlof <contact@gistyr.dev>
2025-12-31 09:50:23 -0500

    impl display

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,13 @@ impl ConversionError {
         return Self::Error(from.into(), msg.into());
     }
 }
+impl std::fmt::Display for ConversionError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ConversionError::Error(from, msg) => write!(f, "Error({}, {})", from, msg),
+        }
+    }
+}
 // ERROR //
 
 // TRAITS //
